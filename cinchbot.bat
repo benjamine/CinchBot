acc871732cfd899de2f4c76350130623102d86e0
Benjamin Eidelman <beneidel@gmail.com>
2014-02-28 22:33:38 -0300

    irc as default adapter

--- cinchbot.bat
+++ cinchbot.bat
@@ -1,1 +1,1 @@
-forever start --minUptime 5000 --spinSleepTime 5000 ./node_modules/coffee-script/bin/coffee ./cinchbot.coffee -a skype
+forever start --minUptime 5000 --spinSleepTime 5000 ./node_modules/coffee-script/bin/coffee ./cinchbot.coffee -a irc
